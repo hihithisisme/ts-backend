3b0008487a7e3c41587e04bc29c830823fd6ae0b
hihithisisme <soong.lexuan6@gmail.com>
2020-08-31 23:21:57 +0800

    feat: proof of concept for in-code db connection

---
+++ src/config/db/postgres/config.ts
@@ -0,0 +1,41 @@
+import pg from 'pg';
+// require('dotenv').config();
+
+interface IpgConfig {
+  user: string,
+  password: string,
+  database: string,
+  host: string,
+  port: number,
+}
+
+const pgConfig = {
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+} as unknown as IpgConfig;
+
+const pool = new pg.Pool(pgConfig);
+
+async function getClient(): Promise<pg.PoolClient> {
+  const client = await pool.connect();
+  return client;
+}
+
+export async function execAndCommit(query: string, value: any[]): Promise<pg.QueryResult> {
+  const client = await getClient();
+
+  try {
+    await client.query('BEGIN');
+    const result = await client.query(query, value);
+    await client.query('COMMIT');
+    return result;
+  } catch (err) {
+    await client.query('ROLLBACK');
+    throw err;
+  } finally {
+    client.release();
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
-const hello_world = {
-  hello: 'world',
-  what: 'up',
-};
+import { execAndCommit } from './config/db/postgres/config';
 
-console.log(hello_world);
+(async () => {
+  
+  console.log('drop');
+  try {
+    await execAndCommit(`
+    DROP TABLE newtable;`,[]);
+  } catch (err) {
+    console.log("ah wells, no table to drop")
+  }
+  console.log('create');
+  await execAndCommit(`
+    CREATE TABLE newtable (
+      test varchar NULL
+    );`, []
+  );
+  console.log('done');
+})();
